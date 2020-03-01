a757c41f36b734297db77c17fee9e8afd6a44292
Al.exe <al_exe@ukr.net>
2020-03-01 21:29:55 +0200

    first commit

---
+++ README.md
@@ -0,0 +1,1 @@
+Rusnak website
---
+++ js/script.js
@@ -0,0 +1,11 @@
+$('.header').on('click','.btn', function () {
+	var top = $('.second').offset().top;
+	$('body,html').animate({scrollTop: top}, 1000);
+});
+
+$('.header').on('click','.menu__item_book', function () {
+	var top = $('.second').offset().top;
+	$('body,html').animate({scrollTop: top}, 1000);
+});
+
+$('.')
