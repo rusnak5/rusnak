a0ad79e07614ebe52c04d5c6875a97479612a2e4
Al.exe <al_exe@ukr.net>
2020-03-07 23:09:11 +0200

    first screen final1

--- js/script.js
+++ js/script.js
@@ -1,9 +1,9 @@
 $('.header').on('click','.btn', function () {
-	var top = $('.second').offset().top;
+	var top = $('.books').offset().top;
 	$('body,html').animate({scrollTop: top}, 1000);
 });
 
 $('.header').on('click','.menu__item_book', function () {
-	var top = $('.second').offset().top;
+	var top = $('.books').offset().top;
 	$('body,html').animate({scrollTop: top}, 1000);
 });
