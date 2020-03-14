31f6f97ad98fd83a445ef057645604a4d22a3118
Al.exe <al_exe@ukr.net>
2020-03-14 16:30:41 +0200

    first screen height + about page

--- js/script.js
+++ js/script.js
@@ -1,14 +1,18 @@
-$('.btn').on('click', function () {
-	var top = $('.books').offset().top;
-	$('body,html').animate({scrollTop: top}, 1000);
-});
+if (window.location.href.indexOf('index.html') != -1) {
+	$('.btn').on('click', function () {
+		var top = $('.books').offset().top;
+		$('body,html').animate({scrollTop: top}, 1000);
+	});
 
-$('.menu__item_book').on('click', function () {
-	var top = $('.books').offset().top;
-	$('body,html').animate({scrollTop: top}, 1000);
-});
+	$('.menu__item_book').on('click', function () {
+		var top = $('.books').offset().top;
+		$('body,html').animate({scrollTop: top}, 1000);
+	});
+}
 
-$('.menu__item_about').on('click', function () {
-	var top = $('.about').offset().top;
-	$('body,html').animate({scrollTop: top}, 1000);
-});
+if (window.location.href.indexOf('about.html') != -1) {
+	$('.btn_about').on('click', function () {
+		var top = $('.about').offset().top;
+		$('body,html').animate({scrollTop: top}, 1000);
+	});
+}
