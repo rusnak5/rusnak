6963efe1742dbbad95425af7c71d2deef84ad908
Al.exe <al_exe@ukr.net>
2020-03-17 23:16:36 +0200

    feedback

--- js/script.js
+++ js/script.js
@@ -26,10 +26,19 @@ $('.menu__item_resp').click(function() {
     $('.resp').fadeToggle(500);
 });
 
-$('.resp__popup').click(function() {
-    $('.resp').fadeToggle(500);
+$('.menu__item_feedback').click(function() {
+	$('.feedback').fadeToggle(500);
 });
 
-$('.resp__close, .resp__wrapper').click(function() {
-    $('.resp').fadeOut(0);
+$('.resp__close, .resp__wrapper, .feedback__close, .feedback__wrapper').click(function() {
+    $('.resp, .feedback').fadeOut(0);
+});
+
+$('.feedback__form').on('submit', function(e){
+    e.preventDefault();
+    var subject = $('.feedback__subject').val();
+    var text = $('.feedback__text').val();
+    var url = 'http://rusnak5.000webhostapp.com' + '?subject=' + subject + '&text=' + text;
+    // document.location.href = url;
+	window.open(url,'policyWindow','height=300px,left=0,top=1,width=500px');
 });
