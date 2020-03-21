eff903d47dd184d3cc87ec1d7f32f2c2751b700f
Al.exe <al_exe@ukr.net>
2020-03-21 23:52:36 +0200

    mail window final

--- js/script.js
+++ js/script.js
@@ -34,10 +34,72 @@ $('.resp__close, .resp__wrapper, .feedback__close, .feedback__wrapper').click(fu
     $('.resp, .feedback').fadeOut(0);
 });
 
+$('.feedback__email-input').on('keyup', function() {
+	if ($(this).val().length > 50) {
+		$(this).val( $(this).val().slice(0, 50) );
+	}
+
+	$(this).removeClass('error');
+	$(this).siblings('.feedback__email-error').hide();
+});
+
+$('.feedback__text-textarea').on('keyup', function() {
+	var regexText = /^[a-zA-Zа-яА-Я0-9\s]*$/;
+
+	if ( regexText.test( $(this).val() ) ) {
+		trueResultLength = $(this).val().length;
+	}
+
+	if ( !regexText.test( $(this).val() ) ) {
+		$(this).val( $(this).val().slice(0,trueResultLength) );
+	}
+
+	$(this).removeClass('error');
+	$(this).siblings('.feedback__textarea-error').hide();
+
+	$('.feedback__counter').text( '' +
+		'Cимволов осталось - ' +
+		(750 - $(this).val().length));
+
+	if ($(this).val().length > 750) {
+		$(this).val( $(this).val().slice(0, 750) );
+	}
+});
+
 $('.feedback__form').on('submit', function(e){
     e.preventDefault();
-    var subject = $('.feedback__subject').val();
-    var text = $('.feedback__text').val();
-    var url = 'http://rusnak5.000webhostapp.com' + '?subject=' + subject + '&text=' + text;
-	window.open(url,'policyWindow','height=300px,left=0,top=1,width=500px');
+
+	var popupEmail = $('.feedback__email-input');
+	var regexEmail = /\S+@\S+\.\S+/;
+	var resultEmail = regexEmail.test( popupEmail.val() );
+	var permissionEmail = false;
+
+	if (resultEmail) {
+		permissionEmail = true;
+		$(popupEmail).removeClass('error');
+		$(popupEmail).siblings('.feedback__email-error').hide();
+	} else {
+		$(popupEmail).addClass('error');
+		$(popupEmail).siblings('.feedback__email-error').show();
+	}
+
+	var popupText = $('.feedback__text-textarea');
+	var permissionText = false;
+
+	if (popupText.val() != '') {
+		permissionText = true;
+		$(popupText).removeClass('error');
+		$(popupText).siblings('.feedback__textarea-error').hide();
+	} else {
+		$(popupText).addClass('error');
+		$(popupText).siblings('.feedback__textarea-error').show();
+	}
+
+	if (permissionEmail && permissionText) {
+		var email = $('.feedback__email-input').val();
+		var text = $('.feedback__text-textarea').val();
+		var url = 'http://rusnak5.000webhostapp.com' + '?email=' + email + '&text=' + text;
+		window.open(url,'policyWindow','height=300px,left=0,top=1,width=500px');
+		$('.feedback').fadeOut(0);
+	}
 });
